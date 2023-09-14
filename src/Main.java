a41cd5f04fe3a9af2fd747f7cdb818fc2d29f187
First Commit <panykosl@mail.uc.edu>
2023-09-14 13:38:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 30;
+        double finalPrice =0;
+        finalPrice = price * 1.05;
+        System.out.println("The price of " + price + " and sales tax is " + finalPrice);
+    }
+
+}
